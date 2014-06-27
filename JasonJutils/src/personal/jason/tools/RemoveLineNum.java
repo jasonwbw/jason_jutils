24a80f53494eb07d33852882beda11b15949b43e
jasonwbw <384444165@163.com>
2014-06-27 11:12:27 +0800

    tools for fitler line number in some codes

---
+++ JasonJutils/src/personal/jason/tools/RemoveLineNum.java
@@ -0,0 +1,33 @@
+package personal.jason.tools;
+
+import java.io.*;
+
+/**
+ * Remove line number in the head of each line for copy code from some website.
+ * 
+ * @author Jasonwbw@yahoo.com
+ */
+
+public class RemoveLineNum {
+	
+	private String strSourceCode="";
+	private BufferedReader br;
+	
+	public RemoveLineNum(String strFilePath) throws Exception{
+		File fileRead = new File(strFilePath);
+		br = new BufferedReader(new FileReader(fileRead));
+	}
+	
+	public String getSourceCode() throws Exception{
+		String s = br.readLine();
+		while ( s != null) {
+			if(s.matches("^[0-9]+")==false){
+				strSourceCode += s + "\n";
+			}
+			s = br.readLine();
+		}
+		br.close();
+		return strSourceCode;
+	}
+
+}
